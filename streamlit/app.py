31d9477103db2158bf5086475a67748fff12d551
ggmxip <92012598+ggmxip@users.noreply.github.com>
2023-06-24 14:27:10 +0530

    added respons

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit as st
 
 # Title
-st.title("My Streamlit App")
+st.title("Welcome Streamlit")
 
 # Text Input
 user_input = st.text_input("Enter your name", "Your name here")
@@ -11,7 +11,18 @@ user_input = st.text_input("Enter your name", "Your name here")
 # Display user input
 st.write("Hello, " + user_input + "!")
 
-
-
+st.markdown(
+    """
+    <style>
+    .stTextInput > div > div > input {
+        width: 100%;
+    }
+    .stButton button {
+        width: 100%;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
