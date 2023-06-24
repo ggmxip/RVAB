cefc71155f12e6b41357ef6bcdde844e6ac75797
ggmxip <92012598+ggmxip@users.noreply.github.com>
2023-06-24 13:25:52 +0530

    streamlit

---
+++ streamlit/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+import streamlit as st
+
+# Title
+st.title("My Streamlit App")
+
+# Text Input
+user_input = st.text_input("Enter your name", "Your name here")
+
+# Display user input
+st.write("Hello, " + user_input + "!")
+
+
+
+
+
